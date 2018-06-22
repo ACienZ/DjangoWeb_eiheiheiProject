6af91264a5a4e13c95e1e69bee0057d73fa8d2e4
ACienZ <986498200@qq.com>
2018-06-22 13:58:22 +0800

    first up to local

---
+++ main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+def MainPage(request):
+    return render(request,'main_app/Mainpage.html')
+
+#将首页跳转到MainPage.html
+def JumpToMainPage(request):
+    return HttpResponseRedirect(reverse('MainPage'))
+
+def test(request):
+    return render(request,'main_app/bottom.html')
